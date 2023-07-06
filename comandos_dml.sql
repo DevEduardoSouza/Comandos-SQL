478d43695e0c4ccce6c2f51af13efc914d543455
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-06 19:42:01 -0300

    Update comandos_dml.sql

--- comandos_dml.sql
+++ comandos_dml.sql
@@ -120,21 +120,5 @@ WHERE length <= 60;
 
 SELECT * FROM payment;
 
-#EXER 04
-#Atualize o "rating" dos filmes da tabela "film" para "G" onde o "length" seja menor ou igual a 60 minutos.
-#Exclua todos os registros da tabela "payment" em que o valor do pagamento seja maior que 10 e a forma de pagamento seja "Cash".
-#Atualize a coluna "phone" da tabela "customer" para "+1-XXX-XXX-XXXX" para todos os clientes que tenham "last_name" igual a "Smith".
-#Exclua todos os registros da tabela "rental" que tenham uma data de aluguel anterior a 2022-01-01 e não tenham sido devolvidos até o momento.
-#Atualize o "email" do cliente com "customer_id" igual a 15 para "novoemail@example.com".
-#Exclua todos os registros da tabela "inventory" que estejam associados a filmes cuja categoria seja "Horror".
-
-
-#EXER 05
-#Atualize a coluna "rental_duration" da tabela "film" para 7 onde o "rating" seja igual a "PG-13".
-#Exclua todos os registros da tabela "payment" em que a forma de pagamento seja "Credit Card" e o valor do pagamento seja inferior a 5.
-#Atualize a coluna "city" da tabela "address" para "Los Angeles" para todos os endereços localizados no estado "California".
-#Exclua todos os registros da tabela "rental" em que a data de aluguel seja anterior a 2022-01-01 e a data de retorno seja posterior a 2022-02-01.
-#Atualize o "email" do cliente com "customer_id" igual a 20 para "novomail@example.com".
-#Exclua todos os registros da tabela "inventory" que estejam associados a filmes que tenham sido lançados antes de 2000 e não estejam mais disponíveis na tabela "film".
 
 
